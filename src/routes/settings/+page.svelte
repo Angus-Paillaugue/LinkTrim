96d11507be2dfd211628f7c9533176d0d3a62feb
mere-patrie <angus.paillaugue40@gmail.com>
2023-07-08 12:04:35 +0200

    a

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+
+    console.log(data)
+</script>
+
+<h1>No settings for now!</h1>
+<p>But here is your user data : </p>
+
+<pre>{JSON.stringify(data.auth, null, 4)}</pre>
